00bc73bde3d340d2ae2eac81a4d0ea9a587d7868
jhon anderson <garciacalvojhonanderson@gmail.com>
2023-02-22 14:37:16 -0500

    este es la primera pwa

--- src/App.js
+++ src/App.js
@@ -1,13 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect} from "react"
 
 function App() {
+  //instalacion
+  const [isReadyForInstall, setIsReadyForInstall] = useState(false);
+
+useEffect(() => {
+  window.addEventListener("beforeinstallprompt", (event) => {
+    // Prevent the mini-infobar from appearing on mobile.
+    event.preventDefault();
+    console.log("👍", "beforeinstallprompt", event);
+    // Stash the event so it can be triggered later.
+    window.deferredPrompt = event;
+    // Remove the 'hidden' class from the install button container.
+    setIsReadyForInstall(true);
+  });
+}, []);
+
+//BOTON DE INSTALACION
+async function downloadApp() {
+  console.log("👍", "butInstall-clicked");
+  const promptEvent = window.deferredPrompt;
+  if (!promptEvent) {
+    // The deferred prompt isn't available.
+    console.log("oops, no prompt event guardado en window");
+    return;
+  }
+  // Show the install prompt.
+  promptEvent.prompt();
+  // Log the result
+  const result = await promptEvent.userChoice;
+  console.log("👍", "userChoice", result);
+  // Reset the deferred prompt variable, since
+  // prompt() can only be called once.
+  window.deferredPrompt = null;
+  // Hide the install button.
+  setIsReadyForInstall(false);
+}
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <button onClick={downloadApp} >descargar</button>
         </p>
         <a
           className="App-link"
