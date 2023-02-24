bf6355ae2351ccd1b3dff110bf230f836fa3a061
jhon anderson <garciacalvojhonanderson@gmail.com>
2023-02-24 12:43:02 -0500

    se agrego jwt

--- src/login.js
+++ src/login.js
@@ -2,8 +2,7 @@ const express=require("express");
 const login=express.Router();
 const bcrypt=require("bcryptjs");
 const coneccion=require("./controllers/conexion");
-
-
+const jwt = require("jsonwebtoken");
 
 
 
@@ -11,16 +10,7 @@ const coneccion=require("./controllers/conexion");
 login.post("/", async (req, res)=>{
     const user= await req.body.usuario;
     const pass= req.body.pass;
-    req.session.usuario=user;
-  //  req.sessionStore=user;
-  if (req.session) {
-    console.log("seccion abierta")
-    
-  }else{
-    console.log("seccion abierta")
-  }
- 
-    
+      
             coneccion.query("SELECT * FROM usuarios WHERE usuario = ?",[user], async (err, result)=>{
                 if(err){
                     res.send("no se encontro ningun usuario")
@@ -36,13 +26,15 @@ login.post("/", async (req, res)=>{
                     const password= await result[0].pass;
                   const comparacion= await bcrypt.compare(pass, password )
                   if (comparacion) {
-                    req.session.usuario=user
-                    req.session.pass=result[0].id
-                    console.log(req.session)
+                    const userToken={
+                      usuario:user,
+                      id:result[0].id
+                    }
+                    const token_jwt=jwt.sign(userToken, "biblioteca", {expiresIn: "24m"});
                    res.status(200).json({
                     success:true,
                     message:"ingreso correctamente",
-                    user:req.session.usuario
+                    user:token_jwt,
                    });
                 
                   }else{
